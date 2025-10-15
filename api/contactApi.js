ad834e204b1dd1f5e3e7c92ef4d170de9b97ce40
Maakalika <phrishikesh172@gmail.com>
2025-10-15 12:17:12 +0530

    Changed Methods

--- api/contactApi.js
+++ api/contactApi.js
@@ -182,7 +182,7 @@ const userTemplate = (data) => {
 // HANDLER
 // -------------------------
 const handler = async (req, res) => {
-  if (req.method !== "POST") {
+  if (req.method != "POST"||req.method!="post") {
     return res
       .status(405)
       .json({ isSuccess: false, message: "Only POST allowed" });
